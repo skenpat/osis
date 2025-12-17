7ff8bc9345a3ba1e048487f760a94f522ba7d071
Fless <132740827+flessan@users.noreply.github.com>
2025-12-17 12:52:44 +0800

    fix line 9

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_PASS', '06942182-12fb-7b70-8000-ec49bfef5d57');
 define('DB_NAME', 'dbE2UwDvxp5kGjWbgS36cCDe');
 
 // Koneksi Database
- $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASS, DB_NAME);
+ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
