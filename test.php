44d87ac038a000572faad2f420f95d039ae44f29
Fless <132740827+flessan@users.noreply.github.com>
2025-12-17 22:22:23 +0800

    test

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ echo "PHP Version: " . phpversion() . "<br>";
 
 // Test database connection
 try {
-    $conn = new mysqli("localhost", "username", "password", "database");
+    $conn = new mysqli("db.fr-pari1.bengt.wasmernet.com", "218212fb78408000caa68efa941e", "06942182-12fb-7b70-8000-ec49bfef5d57", "dbE2UwDvxp5kGjWbgS36cCDe");
     echo "Database connection successful!";
 } catch (Exception $e) {
     echo "Database error: " . $e->getMessage();
